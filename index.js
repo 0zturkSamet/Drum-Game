e1453fc7cb620579888efae6144607a0830804ca
Samet Öztürk <0zturksamet4901@gmail.com>
2022-07-19 20:26:06 +0200

    updated

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
   document.addEventListener("keypress", function (event) {
     makeSound(event.key);
+    buttonAnimation(event.key);
   });
 
   function makeSound(key) {
@@ -48,5 +50,12 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
         console.log(buttonInnerHTML);
         break;
     }
+  }
+  function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function () {
+      activeButton.classList.remove("pressed");
+    }, 150);
   }
 }
